15183f2fbc0a8891ef3eea7b516b70b1ba3fb793
febbynadilasyahira <fnadilasyahira@gmail.com>
2026-01-09 11:30:07 +0700

    deploy8

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,14 @@
 import mysql from "mysql2/promise";
 
-const db = await mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: Number(process.env.DB_PORT),
-  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
-
-console.log("✅ Database connected");
 
 export default db;
